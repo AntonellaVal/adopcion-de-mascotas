687ed36e3f72d5d8c86f055f0435d9a4d397e607
antonella <ant.valdesr@duocuc.cl>
2024-10-10 20:47:28 -0300

    segunda parte deseñoo

--- src/app/pages/formdaradopcion/formdaradopcion.page.ts
+++ src/app/pages/formdaradopcion/formdaradopcion.page.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formdaradopcion',
   templateUrl: './formdaradopcion.page.html',
   styleUrls: ['./formdaradopcion.page.scss'],
 })
-export class FormdaradopcionPage implements OnInit {
+export class FormdaradopcionPage {
+  formularioAdopcion: FormGroup;
+  mostrarVacunas = false;
+  mostrarCondicionSalud = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    // Inicializar el formulario con validaciones
+    this.formularioAdopcion = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      edad: ['', [Validators.required, Validators.min(0)]],
+      especie: ['', Validators.required],
+      genero: ['', Validators.required],
+      tieneVacunas: ['', Validators.required],
+      vacunas: [''],
+      tieneCondicionSalud: ['', Validators.required],
+      condicionSalud: [''],
+      historia: ['']
+    });
   }
 
+  // Método para cambiar el estado de mostrarVacunas
+  cambioVacunas(event: any) {
+    this.mostrarVacunas = event.detail.value === 'si';
+    if (!this.mostrarVacunas) {
+      this.formularioAdopcion.patchValue({ vacunas: '' });
+    }
+  }
+
+  // Método para cambiar el estado de mostrarCondicionSalud
+  cambioCondicionSalud(event: any) {
+    this.mostrarCondicionSalud = event.detail.value === 'si';
+    if (!this.mostrarCondicionSalud) {
+      this.formularioAdopcion.patchValue({ condicionSalud: '' });
+    }
+  }
+
+  // Método para enviar el formulario
+  enviarFormulario() {
+    if (this.formularioAdopcion.valid) {
+      console.log(this.formularioAdopcion.value);
+      alert('Formulario enviado con éxito');
+    } else {
+      alert('Por favor, complete todos los campos requeridos');
+    }
+  }
+
+  // Método para resetear el formulario
+  resetearFormulario() {
+    this.formularioAdopcion.reset();
+    this.mostrarVacunas = false;
+    this.mostrarCondicionSalud = false;
+  }
 }
